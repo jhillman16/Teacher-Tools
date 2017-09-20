31a588569e69b70bc86818662af0c6e18a9fdea7
jhillman16 <jhillman1@gulls.salisbury.edu>
2017-09-20 08:30:40 -0500

    Github Test

--- Web.php
+++ Web.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Teacher Tools!!</title>
+    <title>
+	"Teacher Tools!"
+</title>
 	<meta charset="utf-8" />
 </head>
 <body>
@@ -9,6 +11,11 @@
 <h1>
 HELLO WORLD!
 </h1>
+
+<h2>1. __LINE__</h2>
+<?php
+echo__LINE__;
+?>
 
 </body>
 </html>
