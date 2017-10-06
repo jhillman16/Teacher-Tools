88d76eb0e0875f72343a254fb59d6355bdd93e24
malcmalc3 <malcolm.milton23@yahoo.com>
2017-10-06 14:48:48 -0400

    d

--- Signup.php
+++ Signup.php
@@ -42,7 +42,7 @@
 </form>
 
 <?php
- $db = mysqli_connect('http://acadweb1.salisbury.edu/~mmilton1/connect.php','mmilton1','mmilton1','mmilton1DB')
+ $db = mysqli_connect('http://acadweb1.salisbury.edu/~mmilton1/connect.php','mmilton1','malcmalc3','mmilton1DB')
  or die('Error connecting to MySQL server.');
 ?>
 
