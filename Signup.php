b32c850221ffcac5ce2aa0e8d7168ad4839d5f1e
malcmalc3 <malcolm.milton23@yahoo.com>
2017-10-09 13:14:46 -0400

    m

--- Signup.php
+++ Signup.php
@@ -12,6 +12,21 @@
 <h1> Welcome </h1>
 <h2> Sign Up </h2>
 
+<?php
+$link = mysqli_connect("localhost", "mmilton1", "mmilton1", "mmilton1DB");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+?>
 
 <body background="images/mi.jpg">
 
@@ -40,22 +55,6 @@
     <button type="submit" class="pure-button pure-button-primary">Confirm</button>
     
 </form>
-
-<?php
-$link = mysqli_connect("localhost", "mmilton1", "mmilton1", "mmilton1DB");
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
-?>
 
 </body>
 
