e2207bd6c2a4a72853ccd364731b38b12962926f
malcmalc3 <malcolm.milton23@yahoo.com>
2017-10-09 13:01:26 -0400

    n

--- Signup.php
+++ Signup.php
@@ -38,22 +38,30 @@
     <input type="radio" name="gender" value="teacher"> Educator<br><br>
     
     <button type="submit" class="pure-button pure-button-primary">Confirm</button>
-
-
-    
     
 </form>
 
 <?php
- $db = mysqli('http://acadweb1.salisbury.edu/phpmyadmin','mmilton1','mmilton1','mmilton1DB')
- or die('Error connecting to MySQL server.');
 
- $query="SELECT * FROM StudentsUser";
+ini_set('display_errors', 1);
+error_reporting(E_ALL & E_NOTICE);
+
+if($connection=@mysql_connect("http://acadweb1.salisbury.edu/~mmilton1/connect.php", "mmilton1", "mmilton1")){
+    <p>Successfully connected to MYSQL.</p>
+}else{
+    die('<p>Could not connect to MYSQL because:<b>'.mysql_error().'</b></p>');
+}
+
+if(@mysql_select_db("mmilton1DB", $connection)){
+    print '<p>The mmilton1DB has been selected</p>';
+}
+
+$query="SELECT * FROM StudentsUser";
 if($r=mysql_query($query)){
     echo "dummy1\n";
     print "<p> {$row['FirstName']} </p>\n";
 }
- mysql_close();
+
 ?>
 
 </body>
