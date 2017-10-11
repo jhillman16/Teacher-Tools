09ef704d75e4553439c71c69a85caa06f23807ff
nphbennett <31700858+nphbennett@users.noreply.github.com>
2017-10-11 08:26:27 -0400

    it was the semi-colon all along

--- indexTEST2.php
+++ indexTEST2.php
@@ -29,7 +29,7 @@
 <header>
 	<h1>Welcome Page</h1>
 </header>
-
+<div id="content">
 	<h2>Welcome to Teacher Tools!</h2>
     <p>If you are an Educator and want to create and teach a course on any subject and any difficulty,
 	this tool can help you! <a href="Signup.php">LogIn</a></p>
@@ -38,7 +38,7 @@
 	<br>
 	<p>If you are a Student taking a course, this tool will help you succeed! <a href="Signup.php">LogIn</a></p>
 	
-
+</div>
 </section>
 
 <!--
