2698c1cec9f90ffd3ac3979409add68e5cd7eb5e
nphbennett <31700858+nphbennett@users.noreply.github.com>
2017-10-13 09:40:27 -0400

    adding text for testing length

--- indexTEST2.php
+++ indexTEST2.php
@@ -11,6 +11,15 @@
 	this tool can help you! <a href="Signup.php">LogIn</a></p>
 	<p>OR</p>
 	<p>If you are a Student taking a course, this tool will help you succeed! <a href="Signup.php">LogIn</a></p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis cursus nisl et semper. Suspendisse gravida convallis ante in placerat. Proin eu ipsum lacus. Donec a lectus risus. Aenean non mattis urna. Donec scelerisque justo sit amet felis pretium, ut posuere dui laoreet. Vestibulum libero lacus, congue sed accumsan quis, ultricies ac lacus. Nunc iaculis maximus tortor, sit amet auctor ante mattis sed. Integer sagittis mauris in ligula eleifend, et vehicula purus ultrices. Sed malesuada egestas rhoncus.</p>
+
+	<p>Aliquam id finibus nulla, eu malesuada dolor. Pellentesque sodales felis eget tincidunt auctor. Donec sed egestas velit. Donec a suscipit felis, quis tincidunt neque. Fusce sed felis sit amet nulla tempor eleifend. In blandit lacus nec neque scelerisque, venenatis faucibus libero tempus. Maecenas vitae justo pellentesque, tincidunt tortor at, posuere augue. Maecenas efficitur mauris quis enim rhoncus, sit amet fermentum augue porta.</p>
+
+	<p>Praesent ultrices sem sed lorem auctor sodales. Phasellus auctor tortor mauris, quis semper sapien pellentesque fringilla. Aliquam felis lorem, tempus eu est sit amet, eleifend scelerisque velit. Vivamus sit amet facilisis augue, sit amet tincidunt nunc. Morbi in dapibus tellus. Proin ullamcorper purus turpis, et ullamcorper purus mattis in. Sed massa nisl, finibus finibus placerat et, euismod cursus lectus. Duis vel mi aliquam, porta quam sed, feugiat orci. Sed rutrum luctus turpis et tristique. Aliquam sed nulla id arcu egestas iaculis ac eu turpis. Pellentesque sagittis consectetur dignissim. Suspendisse et ipsum fermentum, venenatis sem sed, ultrices lectus. Quisque tincidunt augue nec nisi porttitor consequat. Ut non scelerisque sapien, eget tristique ante. Nullam et consequat eros, sed laoreet nibh. Proin gravida mauris lectus, ac fermentum metus semper a.</p>
+
+	<p>Cras ornare sem et dictum fermentum. Ut tincidunt, augue ac tincidunt rhoncus, urna nisl rutrum risus, nec feugiat dui nunc id tellus. Cras ultricies nibh eu tempor bibendum. Curabitur vel orci non ligula ultrices viverra non in purus. Pellentesque purus lacus, porta ut nibh et, ornare placerat urna. Pellentesque sit amet massa in ex suscipit eleifend. Vestibulum commodo nibh at nisl egestas, sit amet iaculis mi ornare.</p>
+
+	<p>Nulla tristique gravida sapien vitae mattis. Nam fermentum dolor ac volutpat vestibulum. Suspendisse in gravida lectus. Nam at interdum nibh, sed egestas arcu. Donec nec ligula elit. Mauris viverra, dolor sed maximus mollis, justo purus sollicitudin dolor, quis dapibus elit ligula et massa. Phasellus eu tellus a enim maximus efficitur a ac sem. Etiam diam tortor, venenatis ut volutpat vel, tincidunt mattis mi.</p>
 </div>
 
 </section>
