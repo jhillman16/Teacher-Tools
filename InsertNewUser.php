ee89f923650045d6a8d625ce3235e9f2c4563827
malcmalc3 <malcolm.milton23@yahoo.com>
2017-10-18 08:13:03 -0400

    Updates for Signing Up

--- InsertNewUser.php
+++ InsertNewUser.php
@@ -12,21 +12,28 @@ echo "Success: A proper connection to MySQL was made! The my_db database is grea
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
 // Escape user inputs for security
-$FirstName	= mysqli_real_escape_string($link, $_REQUEST['firstname']);
-$LastName	= mysqli_real_escape_string($link, $_REQUEST['lastname']);
-$UserName	= mysqli_real_escape_string($link, $_REQUEST['username']);
-$Password	= mysqli_real_escape_string($link, $_REQUEST['password']);
-$Email		= mysqli_real_escape_string($link, $_REQUEST['email']);
+$FirstName  = mysqli_real_escape_string($link, $_REQUEST['firstname']);
+$LastName   = mysqli_real_escape_string($link, $_REQUEST['lastname']);
+$UserName   = mysqli_real_escape_string($link, $_REQUEST['username']);
+$Password   = mysqli_real_escape_string($link, $_REQUEST['password']);
+$ConfirmPassword    = mysqli_real_escape_string($link, $_REQUEST['confirm_password']);
+$Email      = mysqli_real_escape_string($link, $_REQUEST['email']);
+$ConfirmEmail       = mysqli_real_escape_string($link, $_REQUEST['confirm_email']);
 $TeacherStudent = mysqli_real_escape_string($link, $_REQUEST['ans']);
- 
+
+if($Password != $ConfirmPassword){
+    header('Location: Signup.php');
+    mysqli_close($link);
+}
+
 // attempt insert query execution
 if($TeacherStudent == "student")
 {
-	$query = "INSERT INTO StudentsUser (Username, Password, FirstName, LastName, Email, JoinedDate) 
+    $query = "INSERT INTO StudentsUser (Username, Password, FirstName, LastName, Email, JoinedDate) 
             VALUES ('$UserName', '$Password', '$FirstName', '$LastName', '$Email', '2017-10-05')";
 }
 elseif ($TeacherStudent == "teacher") {
-	$query = "INSERT INTO TeacherUser (Username, Password, FirstName, LastName, Email, JoinedDate) 
+    $query = "INSERT INTO TeacherUser (Username, Password, FirstName, LastName, Email, JoinedDate) 
             VALUES ('$UserName', '$Password', '$FirstName', '$LastName', '$Email', '2017-10-05')";
 }
 
@@ -36,6 +43,8 @@ if(mysqli_query($link, $query)){
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
  
+header('Location: Creat.html');
+
 // close connection
 mysqli_close($link);
 
