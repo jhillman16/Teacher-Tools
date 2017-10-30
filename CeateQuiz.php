b16df2e77339f7e037b4b6644bac27f05b1f7e7d
evolkis <31695644+evolkis@users.noreply.github.com>
2017-10-30 11:35:31 -0400

    create new quiz

---
+++ CeateQuiz.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create a new quiz</title>
+    <link rel="stylesheet" type="text/css" href="normalize.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+	<meta charset="utf-8" />
+</head>
+<body>
+
+<header>
+	<h1>Create a new quiz</h1>
+
+	<h2>How many questions do you want?</h2>
+
+
+</header>
+
+<form action="qcreat.php" method="post">
+   
+    Number:<input type="number" name="numOfq"><br><br>
+    <input type="submit">
+
+    
+
+</form>
+
+</body>
+</html>
