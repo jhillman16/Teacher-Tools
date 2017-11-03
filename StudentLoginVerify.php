3ee5b162bd21a66a861801262a2a0e3cef2a0794
malcmalc3 <malcolm.milton23@yahoo.com>
2017-11-03 09:30:47 -0400

    Create Course and Login Update

--- StudentLoginVerify.php
+++ StudentLoginVerify.php
@@ -1,42 +1,31 @@
 <?php
-$link = mysqli_connect("localhost", "mmilton1", "mmilton1", "mmilton1DB");
- 
-// Check connection
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+session_start();
+
+include("ConnectDatabase.php"); //Goes through steps of connecting to database
 
 // Escape user inputs for security
 $UserName	= mysqli_real_escape_string($link, $_REQUEST['AdminUsername']);
 $Password	= mysqli_real_escape_string($link, $_REQUEST['StudentPassword']);
 
-//echo "$UserName";
-//echo "$Password";
-
-{
 // attempt get student info
-
-	$query = "SELECT FirstName,LastName FROM StudentsUser WHERE '$UserName'= UserName and '$Password'= Password";
-}
-
-
+$query = "SELECT FirstName, LastName, StudentID FROM StudentsUser WHERE '$UserName'= UserName and '$Password'= Password";
 $result = mysqli_query($link, $query);
 
 if(mysqli_num_rows($result)>0){
+    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $_SESSION['StudentID'] = $row['StudentID'];
+    $_SESSION['FirstName'] = $row['FirstName'];
+    $_SESSION['LastName'] = $row['LastName'];
     echo "Login successful.";
+    mysqli_close($link); // close connection
     header('Location: StuCreate.html');
-} else{
-    echo "ERROR: Incorrect username/password" . mysqli_error($link);
 }
- 
-
-
-// close connection
-mysqli_close($link);
+else
+{
+    echo "ERROR: Incorrect username/password" . mysqli_error($link);
+    mysqli_close($link); // close connection
+    header('Location: StudentLogin.htm');
+}
 
 ?>
