35daad8fead811cfab52176031d7f6b653f93548
nphbennett <31700858+nphbennett@users.noreply.github.com>
2017-11-13 09:33:08 -0500

    another page with new css/php includes

---
+++ Discussion.php
@@ -0,0 +1,35 @@
+<?php include 'header.php';?>
+
+<section>
+<header>
+	<h1>Discussion Board</h1>
+</header>
+
+<div id="content">
+
+
+<table>
+  <tr>
+    <th>Thread Title</th>
+    <th>Replies</th> 
+    <th>Last Modified</th>
+  </tr>
+  <tr>
+    <td><a href="http://www.salisbury.edu">Class tomorrow?</a></td>
+    <td>7</td> 
+    <td>9/26/17 9:37 A.M.</td>
+  </tr>
+  <tr>
+    <td><a href="http://www.salisbury.edu">Today's Homework</a></td>
+    <td>18</td> 
+    <td>9/27/17 3:12 P.M.</td>
+  </tr>
+</table>
+
+
+
+</div>
+
+</section>
+
+<?php include 'footer.php';?>
