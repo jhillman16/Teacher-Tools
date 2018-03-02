9986ed7eb7885d782769e65f25549893aeaa8c73
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-03-01 21:32:43 -0500

    upload changes from semester 2
    playing around on default.php, testing creating a directory, testing displaying content from a variable on main page

---
+++ course/myAssignments.php
@@ -0,0 +1,20 @@
+<?php $title = "My Assignments"; include 'header.php';?>
+
+<table>
+<thead>
+	<tr>
+		<th>Quiz</th>
+		<th>Due Date</th>
+		<th>Points</th>
+	</tr>
+</thead>
+<tbody>
+	<tr>
+		<td><a href="QuizTest.php">Quiz 1</a></td>
+		<td>10/21/2017</td>
+		<td>100</td>
+	</tr>
+</tbody>
+</table>
+
+<?php include 'footer.php';?>
--- default.php
+++ default.php
@@ -1,47 +1,10 @@
 <?php $title = "Teacher Tools"; include 'header.php';?>
-<<<<<<< HEAD
-
-<?php
-
-if(!isset($_SESSION['StudentID']))
-{
-	echo '<p style="text-align: center;"><img alt="logo" src="images/logo.png" /></p>';
-	echo '<p style="text-align: center;"><a href="Signup.php" class="button">Register for your account</a></p>';
-}
-?>
-
-<?php
-if(isset($_SESSION['FirstName']))
-{
-	echo "<p>Welcome, " . $_SESSION['FirstName'] . "!</p>";
-}
-?>
-
-=======
-
-<div style="display: none;">
-<p>What to display on a home page?</p>
-
-<p>Logged in:</p>
-<ul>
-	<li>list of classes
-	<ul>
-		<li>teaching if account flagged educator</li>
-		<li>taking if account flagged student</li>
-	</ul>
-	</li>
-</ul>
-
-<p>Logged out:</p>
-<ul>
-	<li>description of website</li>
-	<li>registration/login link</li>
-</ul>
-</div>
 
 <p style="text-align: center;"><img alt="logo" src="images/logo.png" /></p>
 
-<p style="text-align: center;"><a href="Signup.php" class="button">Register for your account</a></p>
->>>>>>> c9d0c128aa5ebf91d81590e4281aeff8b339bb38
+<?php // no need for register button if account is logged in
+if(!isset($_SESSION['StudentID']) && (!isset($_SESSION['TeacherID'])))
+echo '<p style="text-align: center;"><a href="Signup.php" class="button">Register for your account</a></p>';
+?>
 
 <?php include 'footer.php';?>
