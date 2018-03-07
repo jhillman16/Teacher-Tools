6da20a4fc22e9716411309ae58fbb5666a15feb6
evolkis <31695644+evolkis@users.noreply.github.com>
2018-03-07 17:17:26 -0500

    Add files via upload

---
+++ ViewQuiz.php
@@ -0,0 +1,46 @@
+
+<?php include("ConnectDatabase.php");
+include 'header.php'; //Goes through steps of connecting to database 
+?>
+
+
+<?php
+ /*session_start();
+if( isset( $_SESSION['n'] ) ) {
+      $_SESSION['n'] += 1;
+   }else {
+      $_SESSION['n'] = 0;
+   }*/
+
+$QuestionQuery = "SELECT UserName FROM StudentsUser WHERE StudentID IN(SELECT StudentID FROM `Enrollment` WHERE CourseID = 7)";
+
+
+$r1=mysqli_query($link, $QuestionQuery);
+ if(! $r1 ) {
+      die('Could not get data: ' . mysql_error());
+  }
+while($QuestionRow=mysqli_fetch_array($r1))
+	{
+echo "<html>
+
+
+ <p> Student: {$QuestionRow[0]}</p>  
+ 
+
+ </html>" ;
+
+}
+
+
+
+
+
+
+
+
+
+?>
+
+<?php  
+
+include 'footer.php';?>
