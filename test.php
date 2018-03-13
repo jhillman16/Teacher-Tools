90ce574ae2cb89f496901a6a1aaa6cf0fe3aed7e
JackHillmanGoldenwolf <jwhillman@goldenwolf.com>
2018-03-13 10:39:19 -0400

    read

--- test.php
+++ test.php
@@ -5,13 +5,20 @@ include("ConnectDatabase.php"); //Goes through steps of connecting to database
 
 
 // attempt get teacher info
-$query = "INSERT INTO Test(Message) VALUES('ELi is bailing')";
+$query = "INSERT INTO Test(Message) VALUES('What a day')";
 
 $result = mysqli_query($link, $query); 
+
+$sql = "SELECT Message FROM Test";
+$result2 = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["Message"];
 
 // close connection
 mysqli_close($link);
 
-header('Location: myClass.php');
 
 ?>
