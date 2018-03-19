91c8cf800ce5801511bc5a866ecb4d2af36107d8
malcmalc3 <malcolm.milton23@yahoo.com>
2018-03-19 18:38:15 -0400

    j

--- QuizTest.php
+++ QuizTest.php
@@ -78,35 +78,34 @@ font-style: italic;
 
 	$QuestionQuery = "SELECT Question, QuestionID FROM Question WHERE QuizID = $QuizID";
 	
-	echo "hello";
 	if($r1=mysqli_query($link, $QuestionQuery))
 	{
 		
 		while($QuestionRow=mysqli_fetch_array($r1))
 		{
-				echo "<p class='question'>" . ($QuestionRow['QuestionID'] +1) . ". " . $QuestionRow['Question'] . "</p>";
-				echo "<ul class='answers'>";
+			echo "hello";
+			echo "<p class='question'>" . ($QuestionRow['QuestionID'] +1) . ". " . $QuestionRow['Question'] . "</p>";
+			echo "<ul class='answers'>";
 
-				$QuestionID = $QuestionRow['QuestionID'];
-				$GroupName = 'q' . $QuestionRow['QuestionID']; //These radio buttons for the answers will be grouped by the question number.
-				$Letter = 'a';
+			$QuestionID = $QuestionRow['QuestionID'];
+			$GroupName = 'q' . $QuestionRow['QuestionID']; //These radio buttons for the answers will be grouped by the question number.
+			$Letter = 'a';
 
-				$ResponseQuery = "SELECT ResponseID, IsCorrect, Response FROM Response WHERE QuizID = $QuizID AND QuestionID = $QuestionID";
-				$r2=mysqli_query($link, $ResponseQuery);
-				
-				while($ResponseRow=mysqli_fetch_array($r2))
-				{
-					$AnsID = $GroupName . $Letter;
-					echo '<input type="radio" name=' . $GroupName . ' value=' . $Letter . ' id=' . $AnsID . ' ><label for=' . $AnsID . '>' . $ResponseRow["Response"] . '</label><br/>';
-					$Letter++;
-				}
+			$ResponseQuery = "SELECT ResponseID, IsCorrect, Response FROM Response WHERE QuizID = $QuizID AND QuestionID = $QuestionID";
+			$r2=mysqli_query($link, $ResponseQuery);
+			
+			while($ResponseRow=mysqli_fetch_array($r2))
+			{
+				$AnsID = $GroupName . $Letter;
+				echo '<input type="radio" name=' . $GroupName . ' value=' . $Letter . ' id=' . $AnsID . ' ><label for=' . $AnsID . '>' . $ResponseRow["Response"] . '</label><br/>';
+				$Letter++;
+			}
 
 				echo "</ul>";
 		}
 	}
 	else
 	{
-		echo "hello";
 		echo "ERROR: Not able to execute $sql. " . mysqli_error($link);
 	}
 
