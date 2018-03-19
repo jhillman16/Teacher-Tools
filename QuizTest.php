7819ab182b15f259b69692d10031af8154eb68ea
malcmalc3 <malcolm.milton23@yahoo.com>
2018-03-19 18:46:03 -0400

    j

--- QuizTest.php
+++ QuizTest.php
@@ -78,35 +78,28 @@ font-style: italic;
 
 	$QuestionQuery = "SELECT Question, QuestionID FROM Question WHERE QuizID = $QuizID";
 	
-	if($r1=mysqli_query($link, $QuestionQuery))
+	$r1=mysqli_query($link, $QuestionQuery))
+
+	while($QuestionRow=mysqli_fetch_array($r1))
 	{
+		echo "<p class='question'>" . ($QuestionRow['QuestionID'] +1) . ". " . $QuestionRow['Question'] . "</p>";
+		echo "<ul class='answers'>";
+
+		$QuestionID = $QuestionRow['QuestionID'];
+		$GroupName = 'q' . $QuestionRow['QuestionID']; //These radio buttons for the answers will be grouped by the question number.
+		$Letter = 'a';
+
+		$ResponseQuery = "SELECT ResponseID, IsCorrect, Response FROM Response WHERE QuizID = $QuizID AND QuestionID = $QuestionID";
+		$r2=mysqli_query($link, $ResponseQuery);
 		
-		while($QuestionRow=mysqli_fetch_array($r1))
+		while($ResponseRow=mysqli_fetch_array($r2))
 		{
-			echo $QuestionRow['QuestionID'];//echo "hello";
-			echo "<p class='question'>" . ($QuestionRow['QuestionID'] +1) . ". " . $QuestionRow['Question'] . "</p>";
-			echo "<ul class='answers'>";
-
-			$QuestionID = $QuestionRow['QuestionID'];
-			$GroupName = 'q' . $QuestionRow['QuestionID']; //These radio buttons for the answers will be grouped by the question number.
-			$Letter = 'a';
-
-			$ResponseQuery = "SELECT ResponseID, IsCorrect, Response FROM Response WHERE QuizID = $QuizID AND QuestionID = $QuestionID";
-			$r2=mysqli_query($link, $ResponseQuery);
-			
-			while($ResponseRow=mysqli_fetch_array($r2))
-			{
-				$AnsID = $GroupName . $Letter;
-				echo '<input type="radio" name=' . $GroupName . ' value=' . $Letter . ' id=' . $AnsID . ' ><label for=' . $AnsID . '>' . $ResponseRow["Response"] . '</label><br/>';
-				$Letter++;
-			}
-
-				echo "</ul>";
+			$AnsID = $GroupName . $Letter;
+			echo '<input type="radio" name=' . $GroupName . ' value=' . $Letter . ' id=' . $AnsID . ' ><label for=' . $AnsID . '>' . $ResponseRow["Response"] . '</label><br/>';
+			$Letter++;
 		}
-	}
-	else
-	{
-		echo "ERROR: Not able to execute $sql. " . mysqli_error($link);
+
+			echo "</ul>";
 	}
 
 	
