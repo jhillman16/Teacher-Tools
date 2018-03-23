3dd8a782388a638cf6cd6c5e396444424e742ce2
malcmalc3 <malcolm.milton23@yahoo.com>
2018-03-23 12:42:05 -0400

    j

--- SetClassID.php
+++ SetClassID.php
@@ -13,6 +13,9 @@ $_SESSION['CourseName'] = $_COOKIE['CourseName'];
 unset($_COOKIE['CourseID']);
 unset($_COOKIE['CourseName']);
 
-header('Location: myClass.php');
+$_SESSION['CourseID'] = $_COOKIE['CourseID'];
+$_SESSION['CourseName'] = $_COOKIE['CourseName'];
+
+//header('Location: myClass.php');
 
 ?>
