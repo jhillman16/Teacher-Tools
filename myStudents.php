bab06f4d06f4bd663eceaaf0c92d2581e5e82e3f
malcmalc3 <malcolm.milton23@yahoo.com>
2018-03-23 13:18:48 -0400

    j

--- myStudents.php
+++ myStudents.php
@@ -22,6 +22,7 @@ include("ConnectDatabase.php"); //Goes through steps of connecting to database
 
 echo $_SESSION['$CourseID'];
 echo $_SESSION['$CourseName'];
+echo "hello";
 echo "<h2>Select a student to view performance in course.</h2><br>";
 
 $CourseID = $_SESSION['$CourseID'];
