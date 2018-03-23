43c0875440c9306f2373f23020d0bf530ee0bc35
malcmalc3 <malcolm.milton23@yahoo.com>
2018-03-23 12:39:54 -0400

    j

--- myStudents.php
+++ myStudents.php
@@ -10,7 +10,7 @@ function myFunction(studentID)
 
 <?php
 session_start();
-$title = "My Students - \"" . $_SESSION['$CourseName']; 
+$title = "My Students - " . $_SESSION['$CourseName']; 
 include 'header.php';
 
 if(!isset($_SESSION['CourseID']) || !isset($_SESSION['TeacherID']))
@@ -32,7 +32,7 @@ if($r=mysqli_query($link, $query))
 	while($row=mysqli_fetch_array($r))
 	{
 		echo "<button class='button' onclick='myFunction(" . $row['StudentID'] . ")'>"
-			 . $row['FirstName'] . " " . $row['LastName'] . " (" . $row['UserName'] . ")</button><br><br>";    		
+			 . $row['FirstName'] . " " . $row['LastName'] . " " . $row['UserName'] . "</button><br><br>";    		
 	}
 }
 else
