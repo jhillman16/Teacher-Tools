ba624de6ac52a465b5400f551cf681d240a1dc5b
malcmalc3 <malcolm.milton23@yahoo.com>
2018-03-23 11:56:37 -0400

    j

--- myStudents.php
+++ myStudents.php
@@ -20,10 +20,10 @@ if(!isset($_SESSION['CourseID']) || !isset($_SESSION['TeacherID']))
 
 include("ConnectDatabase.php"); //Goes through steps of connecting to database
 
-echo "<h2>Select a student to view performance in course.<h2><br>";
+echo "<h2>Select a student to view performance in course.</h2><br>";
 echo $_SESSION['$CourseName'];
 
-$query = "SELECT UserName, FirstName, LastName, StudentID FROM StudentsUser WHERE StudentID IN (SELECT StudentID FROM Enrollment WHERE CourseID = $CourseID)";
+$query = "SELECT UserName, FirstName, LastName, StudentID FROM StudentsUser WHERE StudentID IN (SELECT StudentID FROM Enrollment WHERE CourseID = '$CourseID')";
 
 //Attempt to get students's information taking the course
 
