bc46276f9977d373d05dda91585c36117b9d107d
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-03-28 17:29:05 -0400

    test drop down menu

---
+++ testAccordion.php
@@ -0,0 +1,14 @@
+<?php $title = "test menu"; include 'header.php';?>
+
+<p>
+	<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    Button with data-target
+  </button>
+</p>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+  </div>
+</div>
+
+<?php include 'footer.php';?>
