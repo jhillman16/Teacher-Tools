fcd67eebe1cb3cb9b7f3f925a51eab889c150915
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-03-30 11:02:09 -0400

    test own css on accordion

--- testAccordion.php
+++ testAccordion.php
@@ -1,5 +1,6 @@
 <?php $title = "test menu"; include 'header.php';?>
 
+<p>Directly from Bootstrap -- their CSS</p>
 <p>
 	<button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
     Button with data-target
@@ -7,6 +8,18 @@
 </p>
 <div class="collapse" id="collapseExample">
   <div class="card card-body">
+    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+  </div>
+</div>
+
+<p>Doing my own CSS</p>
+<p>
+	<button class="button" type="button" data-toggle="collapse" data-target="#menuLink" aria-expanded="false" aria-controls="menuLink">
+    Button with data-target
+  </button>
+</p>
+<div class="collapse" id="menuLink">
+  <div class="sub-button">
     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
   </div>
 </div>
