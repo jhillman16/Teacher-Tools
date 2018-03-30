a1bac177632a94d35fe7a015cb00818f1648084e
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-03-30 11:10:28 -0400

    test it more like the actual menu

--- testAccordion.php
+++ testAccordion.php
@@ -15,13 +15,23 @@
 <p>Doing my own CSS</p>
 <p>
 	<button class="button" type="button" data-toggle="collapse" data-target="#menuLink" aria-expanded="false" aria-controls="menuLink">
-    Button with data-target
-  </button>
+	Button with data-target
+	</button>
 </p>
 <div class="collapse" id="menuLink">
-  <div class="sub-button">
+	<div class="sub-button">
     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-  </div>
+	</div>
 </div>
+
+<nav>
+	<ul>
+		<li class="accordion-link" data-toggle="collapse" data-target="#subLink" aria-expanded="false" aria-controls="subLink"><a href="#">link</a>
+			<ul class="collapse" id="subLink">
+				<li class="accordion-sub-link"><a href="#">sub-link</a></li>
+			</ul>
+		</li>
+	</ul>
+</nav>
 
 <?php include 'footer.php';?>
