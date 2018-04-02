4683d7eeb6e283f92c10aed56102afdd61b2622d
tholmer1 <31573405+tholmer1@users.noreply.github.com>
2018-04-02 19:59:02 -0400

    kjrb

--- fileUpload.php
+++ fileUpload.php
@@ -1,22 +1,48 @@
+
+
+
+
 <?php
+    $currentDir = getcwd();
+    $uploadDirectory = "/uploads/";
 
-require 'src/Cloudinary.php';
-require 'src/Uploader.php';
-require 'src/Helpers.php'
-require 'src/Api.php'
+    $errors = []; // Store all foreseen and unforseen errors here
 
-\Cloudinary::config(array(
-    "cloud_name" => "hzpnyxrng",
-    "api_key" => "663542711141867",
-    "api_secret" => "3umXJKOqo-t332Cn3njsa-c7Fkc"
-));
+    $fileExtensions = ['jpeg','jpg','png']; // Get all the file extensions
 
-if (isset($_POST["submit"])) {
-    print_r($_FILES["fileToUpload"]);
-    $cloudUpload = \Cloudinary\Uploader::upload($_FILES["fileToUpload"]['tmp_name']);
-    print_r($cloudUpload);
-}
-else'print("Hello its not working");
+    $fileName = $_FILES['myfile']['name'];
+    $fileSize = $_FILES['myfile']['size'];
+    $fileTmpName  = $_FILES['myfile']['tmp_name'];
+    $fileType = $_FILES['myfile']['type'];
+    $fileExtension = strtolower(end(explode('.',$fileName)));
+
+    $uploadPath = $currentDir . $uploadDirectory . basename($fileName); 
+
+    if (isset($_POST['submit'])) {
+
+        if (! in_array($fileExtension,$fileExtensions)) {
+            $errors[] = "This file extension is not allowed. Please upload a JPEG or PNG file";
+        }
+
+        if ($fileSize > 2000000) {
+            $errors[] = "This file is more than 2MB. Sorry, it has to be less than or equal to 2MB";
+        }
+
+        if (empty($errors)) {
+            $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+            if ($didUpload) {
+                echo "The file " . basename($fileName) . " has been uploaded";
+            } else {
+                echo "An error occurred somewhere. Try again or contact the admin";
+            }
+        } else {
+            foreach ($errors as $error) {
+                echo $error . "These are the errors" . "\n";
+            }
+        }
+    }
+
 
 ?>
 
