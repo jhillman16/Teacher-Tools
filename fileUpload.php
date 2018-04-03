580f0e553bec8cca76bb255af46f296ed409be1b
tholmer1 <31573405+tholmer1@users.noreply.github.com>
2018-04-02 20:38:38 -0400

    lsryfiu

--- fileUpload.php
+++ fileUpload.php
@@ -10,14 +10,14 @@ require 'Api.php'
     "api_key" => "663542711141867",
     "api_secret" => "3umXJKOqo-t332Cn3njsa-c7Fkc"
 ));
-\Cloudinary\Uploader::upload("/home/peonies.png");
+\Cloudinary\Uploader::upload("/Tatiana/peonies.png");
 
 if (isset($_POST["submit"])) {
     print_r($_FILES["fileToUpload"]);
     $cloudUpload = \Cloudinary\Uploader::upload($_FILES["fileToUpload"]['tmp_name']);
     print_r($cloudUpload);
 }
-else'print("Hello its not working");
+else print("Hello its not working");
 
 ?>
 
