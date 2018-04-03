bd05b18cc410e74a02bad25841ad2e4a48f6265b
tholmer1 <31573405+tholmer1@users.noreply.github.com>
2018-04-02 21:28:42 -0400

    jkdxjgfi

--- fileUpload.php
+++ fileUpload.php
@@ -1,16 +1,6 @@
 <?php
 
-require 'Cloudinary.php';
-require 'Uploader.php';
-require 'Helpers.php';
-require 'Api.php';
 
-\Cloudinary::config(array(
-    "cloud_name" => "hzpnyxrng",
-    "api_key" => "663542711141867",
-    "api_secret" => "3umXJKOqo-t332Cn3njsa-c7Fkc"
-));
-\Cloudinary\Uploader::upload("/Users/Tatiana/Desktop/peonies.png");
 
 print("Hello!");
 
