e34ccffc90486289bf388e42d3fb9be1cfa33595
tholmer1 <31573405+tholmer1@users.noreply.github.com>
2018-04-03 14:38:56 -0400

    hello

--- UploadFiles.php
+++ UploadFiles.php
@@ -39,8 +39,22 @@ echo 'post_max_size = ' . ini_get('post_max_size') . "\n";
 
 	
 	 <body>
+	 
 <h3>File upload </h3>
 Select a File <BR />
+<li>
+			<span id="accordion">
+				<div id="accordionOne" class="accordion-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+					Site Related Links
+				</div>
+				<div id="collapseOne" class="collapse" aria-labelledby="accordionOne" data-parent="#accordion">
+					<a href="default.php">Upload Link</a>
+					<a href="About.php">Upload Files</a>
+				</div>
+			</span>
+		</li>
+
+
 <form action ="upload.php" method = "post" enctype="multipart/form-data">
 <input type="file" name ="file" size = "500" >
 <input type ="submit" name="T1" value = "Upload File">
