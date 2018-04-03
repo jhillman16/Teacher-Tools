72ae3f87db499311ede02252a5d55d19f7d09a02
tholmer1 <31573405+tholmer1@users.noreply.github.com>
2018-04-02 20:10:47 -0400

    fbf

--- fileUpload.php
+++ fileUpload.php
@@ -34,7 +34,7 @@
             if ($didUpload) {
                 echo "The file " . basename($fileName) . " has been uploaded";
             } else {
-                echo "An error occurred somewhere. Try again or contact the admin" . basename($fileName)." ";
+                echo "An error occurred somewhere. Try again or contact the admin" . basename($fileName)." i dont know ";
             }
         } else {
             foreach ($errors as $error) {
