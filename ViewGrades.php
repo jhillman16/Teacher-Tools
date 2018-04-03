a01f7e4fdc6b23c01ca10e8659ebd018338ca8a9
JackHillmanGoldenwolf <jwhillman@goldenwolf.com>
2018-04-03 10:57:29 -0400

    l

--- ViewGrades.php
+++ ViewGrades.php
@@ -17,13 +17,14 @@ $query = "SELECT Score FROM Performance WHERE StudentID = '$id'";
 
 $grade = '';
 
-$queryQuiz = "SELECT AssignmentID FROM Performance WHERE StudentID = '$id'";
+
 
 $r=mysqli_query($link, $query)
 
 while($row=mysqli_fetch_array($r))
 {
 	$grade = $row[0];
+	$queryQuiz = "SELECT AssignmentID FROM Performance WHERE StudentID = '$id'";
 
 	if($r2=mysqli_query($link, $queryQuiz))
 	{
@@ -33,10 +34,10 @@ while($row=mysqli_fetch_array($r))
 		}
 		else
 		{
-			$row2=mysqli_fetch_array($r2);
-			$assignid = $row2[0];
+			// $row2=mysqli_fetch_array($r2);
+			// $assignid = $row2[0];
 
-			$queryName = "SELECT AssignmentName FROM Assignments WHERE AssignmentID = '$assignid'";
+			// $queryName = "SELECT AssignmentName FROM Assignments WHERE AssignmentID = '$assignid'";
 
 			// if($r3=mysqli_query($link, $queryName))
 			// {
