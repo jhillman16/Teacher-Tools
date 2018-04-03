96bfeb631827349d8f0b37847372d6d1e14bd182
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-04-03 09:47:38 -0400

    testupload

--- testUpload.php
+++ testUpload.php
@@ -2,6 +2,40 @@
 
 
 
+<script src="jquery.min.js" type="text/javascript"></script>
+<script src="jquery.ui.widget.js" type="text/javascript"></script>
+<script src="jquery.iframe-transport.js" type="text/javascript"></script>
+<script src="jquery.fileupload.js" type="text/javascript"></script>
+<script src="jquery.cloudinary.js" type="text/javascript"></script>
+
+<script>
+$('#upload_form').append(
+    // First the "upload preset string", then your cloud name
+    $.cloudinary.unsigned_upload_tag('toofghxs', { cloud_name: 'hzpnyxrng' })
+);
+
+/*
+    Events!
+        - Update a progress bar upon "cloudinaryprogress" event
+        - Add new thumbnail image to gallery when complete!
+*/
+$('.upload_field').unsigned_cloudinary_upload('zcudy0uz', 
+  { cloud_name: 'demo', tags: 'browser_uploads' }, 
+  { multiple: true }
+)
+.bind('cloudinaryprogress', function(e, data) { 
+  $('.progress_bar').css('width', 
+    Math.round((data.loaded * 100.0) / data.total) + '%'); 
+})
+.bind('cloudinarydone', function(e, data) {
+  // Image upload complete!  Add it to gallery with nice animation!
+  $('.thumbnails').append($.cloudinary.image(data.result.public_id, 
+    { format: 'jpg', width: 150, height: 100, 
+      crop: 'thumb', gravity: 'face', effect: 'saturation:50' } ))}
+);
+</script>
+
+
 <!--
 <?php
 
@@ -23,18 +57,18 @@ $fileName = "file_name";
 </form>
 -->
 
-<?php
+<?php /*
 include 'src/Cloudinary.php';
 include 'src/Uploader.php';
-/*\Cloudinary\Uploader::unsigned_upload("sample.jpg", "toofghxs",
-    array("public_id" => "sample_id"));*/
+\Cloudinary\Uploader::unsigned_upload("sample.jpg", "toofghxs",
+    array("public_id" => "sample_id"));
 
 \Cloudinary\Uploader::unsigned_upload("sample.jpg", "toofghxs", 
     array(
     	"cloud_name" => "hzpnyxrng",
     	"public_id" => "sample_id"
     ));
-?>
+*/?>
 
 
 
