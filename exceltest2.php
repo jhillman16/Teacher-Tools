38ff4e3696af19ad2a0a37b2ff95a66060db346c
ErikaFurudo <31659703+ErikaFurudo@users.noreply.github.com>
2018-04-03 11:07:25 -0400

    Update exceltest2.php

--- exceltest2.php
+++ exceltest2.php
@@ -21,8 +21,9 @@ while(!$end)
 		if($column == 1)
 		{
 			echo $data->val($row, $column);
+			$question = $data->val($row, $column);
 			$questionQuery = "INSERT INTO Question (Question, QuizID, QuestionID, Points) 
-            VALUES ($data->val($row, $column), '999', '$QuestionIDNum', '$data->val($row, $column + 1)')";
+            VALUES ('$question', '999', '$QuestionIDNum', '$data->val($row, $column + 1)')";
 			
 			if(mysqli_query($link, $questionQuery))
         			echo "Records added successfully " . $_SESSION['ResponseID'] . ".";
