a75622902141741d536f2104c8bae920fe35c3b4
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-04-03 09:21:07 -0400

    testupload

--- testUpload.php
+++ testUpload.php
@@ -2,7 +2,7 @@
 
 
 
-
+<!--
 <?php
 
 $cloudName = "hzpnyxrng";
@@ -16,11 +16,17 @@ $fileName = "file_name";
 <form action="https://api.cloudinary.com/v1_1/<?php echo $cloudName;?>/image/upload" method="post" enctype="multipart/form-data">
     <label for="file">Filename:</label>
     <input type="file" name="file" id="file"><br>
-    <input type="hidden" name="signature" value="<?php echo sha1('public_id='.$fileName.upload_preset);?>" />
+    <input type="hidden" name="signature" value="<?php echo sha1('public_id='.$fileName.'&timestamp='.$time.$apiSecret);?>" />
     <input type="hidden" name="api_key" value="<?php echo $apiKey; ?>"/>
     <input type="hidden" name="public_id" value="<?php echo $fileName; ?>" />
     <input type="submit" name="submit" value="Submit">
 </form>
+-->
+
+<?php
+\Cloudinary\Uploader::upload("sample.jpg", 
+    array("public_id" => "sample_id"));
+?>
 
 
 
