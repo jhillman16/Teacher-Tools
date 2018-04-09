c29271dc7f22af7e576529c06df8bf7b577041ee
tholmer1 <31573405+tholmer1@users.noreply.github.com>
2018-04-09 16:59:26 -0400

    llkkfh

--- myAssignments.php
+++ myAssignments.php
@@ -10,36 +10,7 @@ function myFunction(AssignmentID)
 }
 </script>
 
-<?php
-    session_start();
-    if(!isset($_SESSION['CourseID']))
-    {
-        header('Location: myClass.php');
-    }
 
-include("ConnectDatabase.php"); //Goes through steps of connecting to database
-
-if( isset($_SESSION['StudentID']) )
-{
-	$CourseID = $_SESSION['CourseID'];
-	$query = "SELECT AssignmentID, AssignmentName, DueDate, CourseID FROM Assignments WHERE CourseID = $CourseID";
-
-	echo "<h2> Quizzes </h2>";
-
-	$r=mysqli_query($link, $query);
-	if(mysqli_num_rows($r) > 0)
-	{
-		while($row=mysqli_fetch_array($r))
-		{
-			echo "<button class='button' onclick='myFunction(" . $row['AssignmentID'] . ")'>"
-			 . $row['AssignmentName'] . ", Due Date: " . $row['DueDate'] . "</button><br><br>";
-		}
-	}
-}
-
-include 'footer.php';
-
-?>
 <?php
 include("ConnectDatabase.php"); //Goes through steps of connecting to database
 
