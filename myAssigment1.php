873ee3e79c02adf52ba0302d116b184f2f853f11
tholmer1 <31573405+tholmer1@users.noreply.github.com>
2018-04-09 17:16:10 -0400

    ,difur

--- myAssigment1.php
+++ myAssigment1.php
@@ -1,6 +1,12 @@
 <?php $title = "My Classes"; include 'header.php';?>
 
 
+<li>
+		<form>
+					<a href="UploadFiles3.php">Upload Files</a>
+					</form></li>
+<li>
+
 
 
 <?php include 'footer.php';?>
