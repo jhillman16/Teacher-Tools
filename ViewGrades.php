cd174ec02122324f19b1e286482cbf5c4a649810
JackHillmanGoldenwolf <jwhillman@goldenwolf.com>
2018-04-10 10:44:03 -0400

    m

--- ViewGrades.php
+++ ViewGrades.php
@@ -35,7 +35,7 @@ while($row=mysqli_fetch_array($r))
 		{
 			$row2=mysqli_fetch_array($r2);
 			$assignid = $row2[0];
-
+			$assignid = $row2[0];
 			$queryName = "SELECT AssignmentName FROM Assignments WHERE AssignmentID = '$assignid'";
 
 			if($r3=mysqli_query($link, $queryName))
