5d467083108a6156c1764625d1329fde13d42d2a
malcmalc3 <malcolm.milton23@yahoo.com>
2018-04-10 11:16:11 -0400

    j

--- Register_For_Class3.php
+++ Register_For_Class3.php
@@ -13,6 +13,7 @@ $StudentID = $_SESSION['StudentID'];
 $CourseID = $_COOKIE['CourseID'];
 unset($_COOKIE['CourseID']);
 
+/*
 //Check if student id already registered for the class
 $query = "SELECT StudentID FROM Enrollment WHERE StudentID = $StudentID AND CourseID = $CourseID";
 $result = mysqli_query($link,$query);
@@ -22,19 +23,20 @@ if(mysqli_num_rows($result)>0)
 	header('Register_For_Class.php');
 	exit();
 }
+*/
 
 //Insert student into the enrollment table for the course
 $query = "INSERT INTO Enrollment (CourseID, StudentID) VALUES ('$CourseID','$StudentID')";
-if(!mysqli_query($link,$query))
+if(mysqli_query($link,$query))
+{
+	$query = "UPDATE Courses SET TotalStudents = TotalStudents + 1 WHERE CourseID = $CourseID";
+	mysqli_query($link,$query);
+}
+else //If the insertion was not successful
 {
 	$_SESSION['error'] = "Could not register for course. Please Try again";
 	header('Register_For_Class.php');
 	exit();
-}
-else //If the insertion was successful
-{
-	$query = "UPDATE Courses SET TotalStudents = TotalStudents + 1 WHERE CourseID = $CourseID";
-	mysqli_query($link,$query);
 }
 
 header('Location: Register_For_Class.php');
