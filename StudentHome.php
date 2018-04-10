185bbe37ee6b13960217d02a6eebaa9e50a6ef41
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-04-10 11:11:31 -0400

    need to be logged in to see home page

--- StudentHome.php
+++ StudentHome.php
@@ -1,8 +1,17 @@
 <?php $title = "Home Page"; include 'header.php';?>
 
-	<p>Welcome to Student's Page</p>
-    	<p>Click "My Classes" to view your classes.</p>
-	<p>Click "My Grades" to view your progress report card. </p>
-	<p>Click "My Assignments" to view you tests, labs, homeworks and more. </p>
+<?php
+if(isset($_SESSION['TeacherID']))
+{
+	echo "<p>Welcome to Student's Page</p>";
+    echo "<p>Click \"My Classes\" to view your classes.</p>";
+	echo "<p>Click \"My Grades\" to view your progress report card. </p>";
+	echo "<p>Click \"My Assignments\" to view you tests, labs, homeworks and more. </p>";
+}
+else
+{
+	header('Location: Login.php');
+}
+?>
 	
 <?php include 'footer.php';?>
