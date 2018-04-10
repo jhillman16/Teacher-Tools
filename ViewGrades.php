32b41a6ee9e738d327065ba990d5a323c7e4b44d
JackHillmanGoldenwolf <jwhillman@goldenwolf.com>
2018-04-10 10:41:44 -0400

    l

--- ViewGrades.php
+++ ViewGrades.php
@@ -43,10 +43,12 @@ while($row=mysqli_fetch_array($r))
 				$row3=mysqli_fetch_array($r3);
 				echo "$row3[0] &nbsp;";
 				echo "<input type='text' placeholder='$grade' name='quizName'> &nbsp;";
-				echo "<button>Submit</button>";
+				echo "<button onclick='myFunction($grade)' >Submit</button>";
 			}
 
 		}
+
+		echo "<br>";
 
 	
 	}	
