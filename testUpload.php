20e0d92ea79dcdaf824412e6494ae7390f6b938d
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-04-11 11:16:38 -0400

    if true

--- testUpload.php
+++ testUpload.php
@@ -20,7 +20,7 @@ echo "<p>You need JavaScript.</p>";
 
 <script>
 	var redir = <?php echo json_encode($php_redir); ?>;
-	if($redir == "1")
+	if($redir)
 		window.location = 'Login.php';
 </script>
 
