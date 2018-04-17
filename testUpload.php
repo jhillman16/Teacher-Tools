0a4c4bdfe6963e817d98d7ca1bd4ab71e6bc998f
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-04-17 09:51:55 -0400

    a test

--- testUpload.php
+++ testUpload.php
@@ -3,7 +3,7 @@
 
 
 
-<?php
+<?php/*
 if(isset($_SESSION['StudentID']) || (isset($_SESSION['TeacherID'])))
 {
 echo '<form enctype="multipart/form-data" action="testUpload-script.php" method="POST">';
@@ -16,12 +16,37 @@ else
 {
 echo '<p>You must be logged in to use this feature.</p>';
 $php_redir = true;
-}
+}*/
 ?>
 
 <script>
-	var redir = <?php echo json_encode($php_redir); ?>;
-	if(redir)
+$(function() {
+// Configure Cloudinary
+// with credentials available on
+// your Cloudinary account dashboard
+$.cloudinary.config({ cloud_name: 'YOUR_CLOUD_NAME', api_key: 'YOUR_API_KEY'});
+// Upload button
+var uploadButton = $('#submit');
+// Upload button event
+uploadButton.on('click', function(e){
+    // Initiate upload
+    cloudinary.openUploadWidget({ cloud_name: 'YOUR_CLOUD_NAME', upload_preset: 'YOUR_UPLOAD_PRESET', tags: ['cgal']}, 
+    function(error, result) { 
+        if(error) console.log(error);
+        // If NO error, log image data to console
+        var id = result[0].public_id;
+        console.log(processImage(id));
+    });
+});
+})
+function processImage(id) {
+var options = {
+    client_hints: true,
+};
+return '<img src="'+ $.cloudinary.url(id, options) +'" style="width: 100%; height: auto"/>';
+}
+//	var redir = <?php echo json_encode($php_redir); ?>;
+//	if(redir)
 		window.location = 'Login.php';
 </script>
 
