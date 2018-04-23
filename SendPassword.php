48a273e3095b6c33490ec0943ea992908e1060ed
malcmalc3 <malcolm.milton23@yahoo.com>
2018-04-22 22:54:02 -0400

    j

--- SendPassword.php
+++ SendPassword.php
@@ -44,7 +44,7 @@
 			$content = new SendGrid\Content("text/plain", "Hello, Email!");
 			$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-			$apiKey = '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';
+			$apiKey = getenv('SENDGRID_API_KEY');
 			$sg = new \SendGrid($apiKey);
 
 			$response = $sg->client->mail()->send()->post($mail);
