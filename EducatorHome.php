1be497481eb2c5c54f8e5e811d3306c8a59bd8af
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-04-25 17:59:54 -0400

    redirect if logged in as student

--- EducatorHome.php
+++ EducatorHome.php
@@ -3,12 +3,18 @@
 <?php
 if(isset($_SESSION['TeacherID']))
 {
-	echo "<p>Welcome to Instructor's Page</p>";
-	echo "<p>Click My files to upload any files for your classes.</p>";
-	echo "<p>Click Create Class to build class on any subject.</p>";
-	echo "<p>Click My Students to view your students.</p>";
-	
-	echo "<p><img src=\"images/class.jpg\" alt=\"Image\" /></p>";
+	echo '<p>Welcome to Instructor\'s Page</p>';
+	echo '<p>Click My files to upload any files for your classes.</p>';
+	echo '<p>Click Create Class to build class on any subject.</p>';
+	echo '<p>Click My Students to view your students.</p>';
+
+	echo '<p><img src="images/class.jpg" alt="Image" /></p>';
+}
+else if (isset($_SESSION['StudentID']))
+{
+	echo '<script>';
+	echo 'window.location.replace("StudentHome.php");';
+	echo '</script>';
 }
 else
 {
