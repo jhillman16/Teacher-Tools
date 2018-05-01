1dc4f266a31e7d4183fccf1866c55881aa85f94f
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-05-01 11:23:04 -0400

    remove header redirect

--- CreateQuiz.php
+++ CreateQuiz.php
@@ -4,18 +4,16 @@ include 'header.php';
 
 if (!isset($_SESSION['StudentID']) && !isset($_SESSION['TeacherID']))
 {
-    echo '<script>';
-    echo 'window.location.replace("Login.php");';
-    echo '</script>';
+	echo '<script>';
+	echo 'window.location.replace("Login.php");';
+	echo '</script>';
 }
 
 if(!isset($_SESSION['CourseID']))
 {
-    header('Location: myClass.php');
-    header_remove();
-    echo '<script>';
-    echo 'window.location.replace("myClass.php");';
-    echo '</script>';
+	echo '<script>';
+	echo 'window.location.replace("myClass.php");';
+	echo '</script>';
 }
 ?>
 
