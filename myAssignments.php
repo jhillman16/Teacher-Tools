513bc18a8fed5bd09ecb14e2c0991a31d8de9897
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-05-01 09:19:56 -0400

    what have I done

--- myAssignments.php
+++ myAssignments.php
@@ -1,6 +1,6 @@
 <?php $title = "My Assignments"; include 'header.php';
 
-if (!isset($_SESSION['StudentID']) || !isset($_SESSION['TeacherID']))
+if (!isset($_SESSION['StudentID']) && !isset($_SESSION['TeacherID']))
 {
 	echo '<script>';
 	echo 'window.location.replace("Login.php");';
