0968d4ac1e6f51c84855f0f4809bd480eb742b6d
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-05-02 09:40:53 -0400

    try cookie clear on login

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function deleteCookie(name)
 {
 	document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
+
+function clearLogin()
+{
+	document.cookie = 'AssignmentID=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
