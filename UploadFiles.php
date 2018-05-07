07e4e62fd65579fb5a1834b8e2e745f643d9f8be
tholmer1 <31573405+tholmer1@users.noreply.github.com>
2018-05-07 18:06:30 -0400

    jdyuidy

--- UploadFiles.php
+++ UploadFiles.php
@@ -3,14 +3,14 @@
 <h2>Choose your next step:</h2>
 
 <ul>
-	<li><a href="UploadFiles3.php">Upload Files</a></li>
-	<li><a href="UploadFiles2.php">Upload Links</a></li>
+	<a class="button" href="UploadFiles3.php">Upload Files</a>
+	<a class="button" href="UploadFiles2.php">Upload Links</a>
 </ul>
 
 <h3>To view your files</h3>
 <a class="button" href="testBLOB.php">Click here  </a>
 
 <h3>To view your links</h3>
-<p><a href="upload2.php">Click here</a></p>
+<a class="button" href="upload2.php">Click here   </a>
 
 <?php include 'footer.php';?>
