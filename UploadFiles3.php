e575c10af37f2f7a963e434bf668ec7e6cdc6a23
nphbennett <31700858+nphbennett@users.noreply.github.com>
2018-05-07 16:12:18 -0400

    code clean-up

--- UploadFiles3.php
+++ UploadFiles3.php
@@ -1,55 +1,25 @@
 <?php $title = "File Upload"; include 'header.php';?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Teacher Tools!!</title>
-    <link rel="stylesheet" type="text/css" href="normalize.css" />
-    
-	<meta charset="utf-8" />
-</head>
-<body>
+<h2>File upload</h2>
 
-
-
-<main>
-
-
-
-<section>
-<header>
-	<h1>Home Page</h1>
-	
-</header>
-
-	
-	 <body>
-<h3>File upload </h3>
 <p style="font-family:courier;">Select File</p>
-<form action = "testBLOB2.php" method= "POST"  enctype = "multipart/form-data">
-      <input type = "file" name = "file"><br><br>
-      <input type = "submit" value = "Submit">
+
+<form action="testBLOB2.php" method="POST"  enctype="multipart/form-data">
+	<div>
+	<label><span>File:</span>
+	<input type="file" id="file" name="file" />
+	</label>
+	</div>
+
+	<div>
+	<input type="submit" value="Submit" />
+	</div>
 </form>
-
-
-
-<form>
-<br><br>
 
 <h3>VIEW YOUR FILES</h3>
-<a href="testBLOB3.php">Click here</a>
-</form>
+<p><a href="testBLOB3.php">Click here</a></p>
 
-<br>
-<br>
 <h5>Upload links</h5>
-<a href="UploadFiles2.php">Click here</a>
+<p><a href="UploadFiles2.php">Click here</a></p>
 
-<body>
-
-
-</main>
-
-</body>
-</html>
 <?php include 'footer.php';?>
