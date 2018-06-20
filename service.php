3877d276fa33cce8bc08fdf32540eb47e3eac6bb
JackHillmanGoldenwolf <jwhillman@goldenwolf.com>
2018-06-20 08:42:01 -0400

    l

--- service.php
+++ service.php
@@ -1,37 +1,16 @@
 <?php
  
-// Create connection
-$con=mysqli_connect("DESKTOP-UVO497A\\sqlexpress","GoldenWolf","Cave27Ace4!","Builder");
- 
-// Check connection
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+// Server in the this format: <computer>\<instance name> or 
+// <server>,<port> when using a non default port number
+$server = 'DESKTOP-UVO497A\SQLEXPRESS';
+
+// Connect to MSSQL
+$link = mssql_connect($server, 'GoldenWolf', 'Cave27Ace4!');
+
+if (!$link) {
+    die('Something went wrong while connecting to MSSQL');
 }
- 
-// This SQL statement selects ALL from the table 'Locations'
-$sql = "SELECT * FROM Test";
- 
-// Check if there are results
-if ($result = mysqli_query($con, $sql))
+else
 {
-	// If so, then create a results array and a temporary one
-	// to hold the data
-	$resultArray = array();
-	$tempArray = array();
- 
-	// Loop through each row in the result set
-	while($row = $result->fetch_object())
-	{
-		// Add each row into our results array
-		$tempArray = $row;
-	    array_push($resultArray, $tempArray);
-	}
- 
-	// Finally, encode the array to JSON and output the results
-	echo json_encode($resultArray);
-}
- 
-// Close connections
-mysqli_close($con);
+	die('We did it');
 ?>
