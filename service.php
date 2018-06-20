d47aa1636bc686956a1258ce51a859ef4c5dbab3
JackHillmanGoldenwolf <jwhillman@goldenwolf.com>
2018-06-20 08:54:31 -0400

    a

--- service.php
+++ service.php
@@ -1,44 +1,28 @@
 <?php
  
-$serverName = "DESKTOP-UVO497A\sqlexpress"; 
-$uid = "GoldenWolf";   
-$pwd = "Cave27Ace4!";  
-$databaseName = "Builder"; 
+include("ConnectDatabase.php");
 
-$connectionInfo = array( "UID"=>$uid,                            
-                         "PWD"=>$pwd,                            
-                         "Database"=>$databaseName); 
-
-/* Connect using SQL Server Authentication. */  
-$conn = sqlsrv_connect( $serverName, $connectionInfo);  
-
-$tsql = "SELECT * FROM Test";  
-
-/* Execute the query. */  
-
-$stmt = sqlsrv_query( $conn, $tsql);  
-
-if ( $stmt )  
-{  
-     echo "Statement executed.<br>\n";  
-}   
-else   
-{  
-     echo "Error in statement execution.\n";  
-     die( print_r( sqlsrv_errors(), true));  
-}  
-
-/* Iterate through the result set printing a row of data upon each iteration.*/  
-
-while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC))  
-{  
-     echo "Col1: ".$row[0]."\n";  
-     echo "Col2: ".$row[1]."\n";  
-     echo "Col3: ".$row[2]."<br>\n";  
-     echo "-----------------<br>\n";  
-}  
-
-/* Free statement and connection resources. */  
-sqlsrv_free_stmt( $stmt);  
-sqlsrv_close( $conn);  
+// This SQL statement selects ALL from the table 'Locations'
+$sql = "SELECT * FROM Courses";
+ 
+// Check if there are results
+if ($result = mysqli_query($link, $sql))
+{
+	// If so, then create a results array and a temporary one
+	// to hold the data
+	$resultArray = array();
+	$tempArray = array();
+ 
+	// Loop through each row in the result set
+	while($row = $result->fetch_object())
+	{
+		// Add each row into our results array
+		$tempArray = $row;
+	    array_push($resultArray, $tempArray);
+	}
+ 
+	// Finally, encode the array to JSON and output the results
+	echo json_encode($resultArray);
+}
+ 
 ?>
