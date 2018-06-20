09610fc897a99ff6ae6de3da78e55ac14f8005a2
JackHillmanGoldenwolf <jwhillman@goldenwolf.com>
2018-06-20 08:49:40 -0400

    a

--- service.php
+++ service.php
@@ -1,17 +1,44 @@
 <?php
  
-// Server in the this format: <computer>\<instance name> or 
-// <server>,<port> when using a non default port number
-$server = 'DESKTOP-UVO497A\SQLEXPRESS';
+$serverName = "DESKTOP-UVO497A"; 
+$uid = "GoldenWolf";   
+$pwd = "Cave27Ace4!";  
+$databaseName = "Builder"; 
 
-// Connect to MSSQL
-$link = mssql_connect($server, 'GoldenWolf', 'Cave27Ace4!');
+$connectionInfo = array( "UID"=>$uid,                            
+                         "PWD"=>$pwd,                            
+                         "Database"=>$databaseName); 
 
-if (!$link) {
-    echo"Something went wrong while connecting to MSSQL";
-}
-else
-{
-	echo"We did it";
-}
+/* Connect using SQL Server Authentication. */  
+$conn = sqlsrv_connect( $serverName, $connectionInfo);  
+
+$tsql = "SELECT * FROM Test";  
+
+/* Execute the query. */  
+
+$stmt = sqlsrv_query( $conn, $tsql);  
+
+if ( $stmt )  
+{  
+     echo "Statement executed.<br>\n";  
+}   
+else   
+{  
+     echo "Error in statement execution.\n";  
+     die( print_r( sqlsrv_errors(), true));  
+}  
+
+/* Iterate through the result set printing a row of data upon each iteration.*/  
+
+while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC))  
+{  
+     echo "Col1: ".$row[0]."\n";  
+     echo "Col2: ".$row[1]."\n";  
+     echo "Col3: ".$row[2]."<br>\n";  
+     echo "-----------------<br>\n";  
+}  
+
+/* Free statement and connection resources. */  
+sqlsrv_free_stmt( $stmt);  
+sqlsrv_close( $conn);  
 ?>
